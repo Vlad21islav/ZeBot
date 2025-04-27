3089a4b44a17c5591bdbe25a0ddb42768f119eb5
Vlad21islav <135822561+Vlad21islav@users.noreply.github.com>
2025-04-27 12:39:16 +0300

    voice

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import telebot
 import requests
 import kandinsky
 from faster_whisper import WhisperModel
+import time
 
 TOKEN = os.environ.get('TOKEN')
 weather_api = os.environ.get('weather_api')
@@ -14,15 +15,18 @@ model = None
 
 if not TOKEN:
     import keys
+
     TOKEN = keys.TOKEN
     weather_api = keys.weather_api
     news_api = keys.news_api
     my_id = keys.my_id
 
 bot = telebot.TeleBot(TOKEN)
-
+bot.remove_webhook()
 
 path = ""
+
+
 # path = "/home/Vlad21islav/ZeBot/"
 
 
@@ -57,163 +61,167 @@ def add_users(message):
         bot.send_message(int(my_id), username)
 
 
-def main():
+@bot.message_handler(commands=["start"])
+def start_command(message):
+    add_users(message)
+    markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+    btn = telebot.types.InlineKeyboardButton("Показать используемые сервисы", callback_data="show services")
+    markup.add(btn)
+    bot.send_message(message.chat.id, "Привет, рад тебя видеть! Введи /help для вывода списка команд", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data == "show services":
+        bot.send_message(call.message.chat.id, f"Для создания бота были использованы такие сервисы как: <b>openweather</b>, <b>kandinsky</b> и <b>newsapi</b>", parse_mode="html")
+
+
+@bot.message_handler(commands=["help"])
+def help_command(message):
+    add_users(message)
+    bot.send_message(message.chat.id, "/help - вывод всех команд\n"
+                                      "/weather - вывод погоды в городе в данное время\n"
+                                      "/kandinsky - генерация картинок по запросу\n"
+                                      "/news - показать текущие новости по теме\n"
+                                      "/predict - ответить на вопрос")
+
+
+@bot.message_handler(commands=["weather"])
+def get_weather_command(message):
+    add_users(message)
+    city = message.text.strip()[(command_length(message) + 1):].title()
+    res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api}&units=metric")
     try:
-        @bot.message_handler(commands=["start"])
-        def start_command(message):
-            add_users(message)
-            markup = telebot.types.InlineKeyboardMarkup(row_width=1)
-            btn = telebot.types.InlineKeyboardButton("Показать используемые сервисы", callback_data="show services")
-            markup.add(btn)
-            bot.send_message(message.chat.id, "Привет, рад тебя видеть! Введи /help для вывода списка команд", reply_markup=markup)
+        bot.reply_to(message,
+                     f"Погода в городе {city}: \n"
+                     f"Температура: {round(res.json()['main']['temp'])}°C\n"
+                     f"Ощущается как: {round(res.json()['main']['feels_like'])}°C\n"
+                     f"Влажность: {round(res.json()['main']['humidity'])}%")
+        bot.send_message(message.chat.id, f"[ㅤ](https://openweathermap.org/img/wn/{res.json()['weather'][0]['icon']}@4x.png)", parse_mode="MarkdownV2")
+    except KeyError:
+        bot.reply_to(message, f"Город не найден, попробуйте ввести другой")
 
-        @bot.callback_query_handler(func=lambda call: True)
-        def callback(call):
-            if call.data == "show services":
-                bot.send_message(call.message.chat.id, f"Для создания бота были использованы такие сервисы как: <b>openweather</b>, <b>kandinsky</b> и <b>newsapi</b>", parse_mode="html")
 
-        @bot.message_handler(commands=["help"])
-        def help_command(message):
-            add_users(message)
-            bot.send_message(message.chat.id, "/help - вывод всех команд\n"
-                                              "/weather - вывод погоды в городе в данное время\n"
-                                              "/kandinsky - генерация картинок по запросу\n"
-                                              "/news - показать текущие новости по теме\n"
-                                              "/predict - ответить на вопрос")
+@bot.message_handler(commands=["news"])
+def get_news_command(message):
+    add_users(message)
+    prompt = message.text.strip()[(command_length(message) + 1):].title()
 
-        @bot.message_handler(commands=["weather"])
-        def get_weather_command(message):
-            add_users(message)
-            city = message.text.strip()[(command_length(message) + 1):].title()
-            res = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api}&units=metric")
-            try:
-                bot.reply_to(message,
-                             f"Погода в городе {city}: \n"
-                             f"Температура: {round(res.json()['main']['temp'])}°C\n"
-                             f"Ощущается как: {round(res.json()['main']['feels_like'])}°C\n"
-                             f"Влажность: {round(res.json()['main']['humidity'])}%")
-                bot.send_message(message.chat.id, f"[ㅤ](https://openweathermap.org/img/wn/{res.json()['weather'][0]['icon']}@4x.png)", parse_mode="MarkdownV2")
-            except KeyError:
-                bot.reply_to(message, f"Город не найден, попробуйте ввести другой")
+    news_params = {
+        'q': 'Россия',  # Запрос по ключевому слову
+        'apiKey': news_api,
+        'language': 'ru',
+    }
 
-        @bot.message_handler(commands=["news"])
-        def get_news_command(message):
-            add_users(message)
-            prompt = message.text.strip()[(command_length(message) + 1):].title()
+    if len(prompt) >= 1:
+        news_params["q"] = prompt
 
-            news_params = {
-                'q': 'Россия',  # Запрос по ключевому слову
-                'apiKey': news_api,
-                'language': 'ru',
-            }
+    # URL для запроса новостей
+    url = 'https://newsapi.org/v2/everything'
+    response = requests.get(url, params=news_params)
 
-            if len(prompt) >= 1:
-                news_params["q"] = prompt
+    if response.status_code == 200:
+        data = response.json()
 
-            # URL для запроса новостей
-            url = 'https://newsapi.org/v2/everything'
-            response = requests.get(url, params=news_params)
+        # Печать заголовков новостей
+        text = ""
+        max_length = 20
+        # Вывод заголовков новостей
+        for article in data["articles"]:
+            text += "\n" * 2 + article["title"]
+            if max_length <= 0:
+                break
+            max_length -= 1
 
-            if response.status_code == 200:
-                data = response.json()
+        bot.send_message(message.chat.id, text)
 
-                # Печать заголовков новостей
-                text = ""
-                max_length = 20
-                # Вывод заголовков новостей
-                for article in data["articles"]:
-                    text += "\n" * 2 + article["title"]
-                    if max_length <= 0:
-                        break
-                    max_length -= 1
+    else:
+        print(f"Ошибка: {response.status_code}")
+        bot.send_message(message.chat.id, f"При обработке комманды произошла ошибка, попробуйте повторить запрос")
 
-                bot.send_message(message.chat.id, text)
 
-            else:
-                print(f"Ошибка: {response.status_code}")
-                bot.send_message(message.chat.id, f"При обработке комманды произошла ошибка, попробуйте повторить запрос")
+@bot.message_handler(commands=["kandinsky"])
+def kandinsky_command(message):
+    add_users(message)
+    prompt = message.text.strip()[(command_length(message) + 1):].title()
+    if len(prompt) >= 1:
+        print(f"--{prompt}--")
+        bot.send_message(message.chat.id, "Изображение генерируется, надо немного подождать")
+        kandinsky.kandinsky(prompt)
+        file = open(path + "image.jpg", "rb")
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.send_message(message.chat.id, "Слишком маленький запрос")
 
-        @bot.message_handler(commands=["kandinsky"])
-        def kandinsky_command(message):
-            add_users(message)
-            prompt = message.text.strip()[(command_length(message) + 1):].title()
-            if len(prompt) >= 1:
-                print(f"--{prompt}--")
-                bot.send_message(message.chat.id, "Изображение генерируется, надо немного подождать")
-                kandinsky.kandinsky(prompt)
-                file = open(path + "image.jpg", "rb")
-                bot.send_photo(message.chat.id, file)
-            else:
-                bot.send_message(message.chat.id, "Слишком маленький запрос")
 
-        @bot.message_handler(commands=["predict"])
-        def predict_command(message):
-            add_users(message)
-            prompt = message.text.strip()[(command_length(message) + 1):].title()
-            chat_id = message.chat.id
-            if len(prompt) >= 1:
-                if prompt[:4] == 'Кто ':
-                    administrators = bot.get_chat_administrators(chat_id)  # Получаем список администраторов
-                    name = 'my_ze_bot'
-                    if len(administrators) > 1:
-                        while name == 'my_ze_bot':
-                            random_admin = random.choice(administrators)  # Выбираем случайного администратора
-                            admin_name = random_admin.user.username  # Имя администратора
+@bot.message_handler(commands=["predict"])
+def predict_command(message):
+    add_users(message)
+    prompt = message.text.strip()[(command_length(message) + 1):].title()
+    chat_id = message.chat.id
+    if len(prompt) >= 1:
+        if prompt[:4] == 'Кто ':
+            administrators = bot.get_chat_administrators(chat_id)  # Получаем список администраторов
+            name = 'my_ze_bot'
+            if len(administrators) > 1:
+                while name == 'my_ze_bot':
+                    random_admin = random.choice(administrators)  # Выбираем случайного администратора
+                    admin_name = random_admin.user.username  # Имя администратора
 
-                            if admin_name:
-                                bot.send_message(chat_id, f"Мне кажется, что @{admin_name} {prompt[4:].split('?')[0].lower()}")
-                                name = admin_name
-                            else:
-                                bot.send_message(chat_id, f"Мне кажется, что {random_admin.user.nickname} {prompt[4:]}")
-                                name = random_admin.user.nickname
+                    if admin_name:
+                        bot.send_message(chat_id, f"Мне кажется, что @{admin_name} {prompt[4:].split('?')[0].lower()}")
+                        name = admin_name
                     else:
-                        bot.send_message(chat_id, "В чате нет администраторов!")
-
-                elif prompt[:4] == 'Где ':
-                    answers = ["В другой реальности, но я не скажу в какой", "Ну точно не там, где нужно", "В пространстве между реальностями"]
-                    bot.reply_to(message, random.choice(answers))
-
-                else:
-                    answers = ["да", "возможно", "нет", "не уверен", "пожалуй", "не думаю", "точно нет", "может быть", "смотря по ситуации", "не знаю", "конечно", "пока не могу сказать"]
-                    bot.reply_to(message, random.choice(answers))
+                        bot.send_message(chat_id, f"Мне кажется, что {random_admin.user.nickname} {prompt[4:]}")
+                        name = random_admin.user.nickname
             else:
-                bot.send_message(message.chat.id, "Слишком маленький запрос")
+                bot.send_message(chat_id, "В чате нет администраторов!")
 
-        @bot.message_handler(content_types=['voice'])
-        def handle_voice(message):
-            try:
-                # Скачиваем голосовое сообщение
-                file_info = bot.get_file(message.voice.file_id)
-                downloaded_file = bot.download_file(file_info.file_path)
+        elif prompt[:4] == 'Где ':
+            answers = ["В другой реальности, но я не скажу в какой", "Ну точно не там, где нужно", "В пространстве между реальностями"]
+            bot.reply_to(message, random.choice(answers))
 
-                # Сохраняем временно файл
-                with open("voice.ogg", 'wb') as new_file:
-                    new_file.write(downloaded_file)
-
-                # Загружаем модель при первом вызове
-                model_instance = get_model()
-
-                # Распознаём речь
-                segments, info = model_instance.transcribe("voice.ogg")
-
-                # Собираем текст из всех сегментов
-                recognized_text = ''
-                for segment in segments:
-                    recognized_text += segment.text + ' '
-
-                # Отправляем текст обратно пользователю
-                bot.reply_to(message, recognized_text.strip())
-
-            except Exception as e:
-                bot.reply_to(message, f"Ошибка при обработке голосового сообщения:\n{str(e)}")
-
-        bot.polling(none_stop=True, timeout=123)
-
-    except requests.exceptions.ReadTimeout:
-        main()
-    except requests.exceptions.ProxyError:
-        main()
+        else:
+            answers = ["да", "возможно", "нет", "не уверен", "пожалуй", "не думаю", "точно нет", "может быть", "смотря по ситуации", "не знаю", "конечно", "пока не могу сказать"]
+            bot.reply_to(message, random.choice(answers))
+    else:
+        bot.send_message(message.chat.id, "Слишком маленький запрос")
 
 
-main()
+@bot.message_handler(content_types=['voice'])
+def handle_voice(message):
+    try:
+        # Скачиваем голосовое сообщение
+        file_info = bot.get_file(message.voice.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
 
+        # Сохраняем временно файл
+        with open("voice.ogg", 'wb') as new_file:
+            new_file.write(downloaded_file)
+
+        # Загружаем модель при первом вызове
+        model_instance = get_model()
+
+        # Распознаём речь
+        segments, info = model_instance.transcribe("voice.ogg")
+
+        # Собираем текст из всех сегментов
+        recognized_text = ''
+        for segment in segments:
+            recognized_text += segment.text + ' '
+
+        # Отправляем текст обратно пользователю
+        bot.reply_to(message, recognized_text.strip())
+
+    except Exception as e:
+        bot.reply_to(message, f"Ошибка при обработке голосового сообщения:\n{str(e)}")
+
+
+bot.polling(none_stop=True, timeout=123)
+
+while True:
+    try:
+        bot.infinity_polling(timeout=123, long_polling_timeout=10)
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        time.sleep(5)
